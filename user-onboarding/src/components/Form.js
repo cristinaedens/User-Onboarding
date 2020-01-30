b9cd62f613625a9a8ac7f87da55b996dbc1e71fc
Cristina Edens <edens.cristina@gmail.com>
2020-01-30 15:15:09 -0500

    File setup with some imports

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {withFormik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+//Things needed{
+//Name, Email, Password, TOS(checkbox), Submit Button
+//}
+
+
