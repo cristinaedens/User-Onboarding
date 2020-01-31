788b2a35ded408a4388bd828a6adfd7b688e46a8
Cristina Edens <edens.cristina@gmail.com>
2020-01-30 19:45:53 -0500

    Completed styles and finished project

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -5,6 +5,9 @@ import NewUser from "./components/Form"
 function App() {
   return (
     <div className="App">
+      <header>
+        User Onboarding.
+      </header>
      <NewUser/>
     </div>
   );
--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -3,6 +3,11 @@ import {withFormik, Form, Field, ErrorMessage} from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 
+
+
+
+
+
 //Things needed{
 //Name, Email, Password, TOS(checkbox), Submit Button
 //}
@@ -25,17 +30,18 @@ return(
         <label>Enter Name<Field type="text" name="name" placeholder="Name"/></label>
         {touched.name && errors.name && (<p className="error">{errors.email}</p>)}
       </div>
-
+      <br/>
       <div className="emailField">
         <label>Enter Email<Field type="email" name="email" placeholder="Email"/></label>
         {touched.email && errors.email && (<p className="error">{errors.email}</p>)}
       </div>
-
+      <br/>
       <div className="passwordField">
         <label>Enter Password<Field type="password" name="password" placeholder="Password"/></label>
         {touched.password && errors.password && (<p className="error">{errors.password}</p>)}
       </div>
-
+      <br/>
+      <br/>
       <div className="tosField">
         <label>I agree to the Terms of Services: <Field type="checkbox" name="terms" checked={values.terms}/></label>
         {touched.terms && errors.terms && (<p className="error">{errors.terms}</p>)}
@@ -43,10 +49,10 @@ return(
       </div>
     </Form>
     {user.map(person => (
-      <ul key={person.id}>
-        <li>Name: {person.name}</li>
-        <li>Email: {person.email}</li>
-    <li>Password: {"*".repeat(person.password.length)}</li>
+      <ul className="returnCard" key={person.id}>
+        <li className="cardName">Name: {person.name}</li>
+        <li className="cardEmail">Email: {person.email}</li>
+    <li className="cardPassword">Password: {"*".repeat(person.password.length)}</li>
       </ul>
     ))}
   </div>
@@ -70,12 +76,13 @@ validationSchema: Yup.object().shape({
   terms: Yup.boolean().oneOf([true], "Please accept Terms of Service.").required()
 }),
 
-handleSubmit(values, {setStatus}) {
+handleSubmit(values, {setStatus, resetForm}) {
   axios
-  .post(" https://reqres.in/api/users/", values)
+  .post("https://reqres.in/api/users/", values)
   .then(response => {
     console.log(response);
     setStatus(response.data);
+    resetForm()
   })
   .catch(error => console.log(error.response));
 }
